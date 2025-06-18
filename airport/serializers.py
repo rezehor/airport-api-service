28dc6874963bfb0ea46f68d97d7679f2e1ba7c4a
Timur Khvat <timurkhvat@gmail.com>
2025-06-18 10:57:30 +0200

    Add row and seat validation to TicketSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from airport.models import (
     AirplaneType,
@@ -87,6 +88,16 @@ class FlightDetailSerializer(FlightSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["flight"].airplane,
+            ValidationError
+        )
+        return data
+
     class Meta:
         model = Ticket
         fields = ("id", "row", "seat", "flight")
